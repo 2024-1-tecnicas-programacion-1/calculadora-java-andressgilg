88c7afbb52e6cc0e4206aac1cd202c7d56151d8b
Davidparra <df.parra.07@gmail.com>
2024-03-04 08:35:54 -0500

    Raiz
    se le agrego la operación de la raíz

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -28,14 +28,18 @@ public class Calculadora {
                 break;
             
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
-            case '^' :
-                // Operación división:
+            case 'p' :
+                // Operación potencia:
                 respuesta = Math.pow (numero1, numero2);
                 break;
             case '%' :
-                // Operación división:
+                // Operación modulo:
                 respuesta = numero1 % numero2;
                 break;    
+             case 'r' :
+                // Operación raiz:
+                respuesta = Math.pow(numero1, (double)1/numero2);
+                break;  
                 
             default:
                 throw new IllegalArgumentException("Operación inválida.");
@@ -54,7 +58,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /, ^ ,% )");
+        System.out.println("Ingrese la operación (+, -, *, /, p ,%,r )");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
